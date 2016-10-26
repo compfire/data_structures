521218e9a1902cc02f4146ad431e9c91a402aadf
Dano Oravec <danoravec@gmail.com>
2016-10-26 14:40:27 +0200

    Expression input evaluator in main

--- main.cpp
+++ main.cpp
@@ -1,24 +1,80 @@
-#include "Sort.h"
 #include <iostream>
+#include <vector>
+#include <stack>
+#include <map>
 #include <cmath>
-#include <ctime>
 
 using namespace std;
 
 
 int main() {
-    Sort sorter;
-    srand(time(NULL));
-    vector<int> nums;
-    for(int i = 0; i < 10; i++) {
-        nums.push_back(rand() % 300 + 200);
-        cout << nums[i] << " ";
+    stack<char> opers;
+    map<char, short> opers_priority;
+    opers_priority['('] = 0;
+    opers_priority[')'] = 0;
+    opers_priority['+'] = 1;
+    opers_priority['-'] = 1;
+    opers_priority['*'] = 2;
+    opers_priority['/'] = 2;
+    opers_priority['^'] = 3;
+    vector<char> res;
+    char c;
+    while((c = getchar()) != '\n') {
+        if(c - '0' <= 9 && c - '0' >= 0) {
+            res.push_back(c);
+            continue;
+        }
+        else if(c == ' ')
+            continue;
+        else {
+            if(c == ')') {
+                while(opers.top() != '(') {
+                    res.push_back(opers.top());
+                    opers.pop();
+                }
+                opers.pop();
+            }
+            else if(opers.empty() || opers_priority[opers.top()] < opers_priority[c] || c == '(')
+                opers.push(c);
+            else {
+                while(!opers.empty() && opers_priority[opers.top()] > opers_priority[c]) {
+                    res.push_back(opers.top());
+                    opers.pop();
+                }
+                opers.push(c);
+            }
+        }
     }
-    cout << endl;
-    sorter.radix_sort(nums);
-    for(int i = 0; i < nums.size(); i++)
-        cout << nums[i] << " ";
+    while(!opers.empty()) {
+        res.push_back(opers.top());
+        opers.pop();
+    }
 
+    stack<double> evaluator;
+    double num1, num2;
+    for(int i = 0; i < res.size(); i++) {
+        c = res[i];
+        if(c - '0' >= 0 && c - '0' <= 9)
+            evaluator.push((double)(c - '0'));
+        else {
+            num2 = evaluator.top();
+            evaluator.pop();
+            num1 = evaluator.top();
+            evaluator.pop();
+            if(c == '+')
+                evaluator.push(num1 + num2);
+            else if(c == '-')
+                evaluator.push(num1 - num2);
+            else if(c == '*')
+                evaluator.push(num1 * num2);
+            else if(c == '/')
+                evaluator.push(num1 / num2);
+            else
+                evaluator.push(pow(num1, num2));
+        }
+    }
+
+    cout << evaluator.top() << endl;
 
     return 0;
 }
